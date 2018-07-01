c7c71a3d78b13700fbe7fcf506024226699e9065
soni24 <bhanusoni24@gmail.com>
2018-07-01 14:50:57 +0530

    Add files via upload

---
+++ Reg/php/stack2.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<?php 
+shell_exec("sh -c script/term.sh &");
+
+?>
+<h3>Stack selected visit Container by below link</h3>
+<a href="http://localhost:4210">Container</a>
+</body>
+</html>
